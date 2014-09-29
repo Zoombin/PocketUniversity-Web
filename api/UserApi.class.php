a7cbf459c3c0cf011ac6abe26fd466823e8639a9
Bin Zhang <30135878@qq.com>
2014-09-29 13:51:46 +0800

    注释掉以后网站菜才能打开

--- api/UserApi.class.php
+++ api/UserApi.class.php
@@ -3,6 +3,11 @@ class UserApi extends Api{
 
 	//按用户UID或昵称返回用户资料，同时也将返回用户的最新发布的微博
 	function show(){
+        // $model = D('User', 'home');
+        // $user  = $model->getUserByIdentifier($uid, $this->user_id);
+        // return $user;
+
+        header('Content-Type: application/json');
 		$data = getUserInfo($this->user_id, urldecode($this->user_name),$this->mid,true);
 		return $data;
 	}
